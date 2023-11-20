c3ead280acdd169fb3d6739113f9967853601d5f
edcoelho <edcoelho.dev@gmail.com>
2023-11-20 00:31:23 -0300

    Adiciona suporte a adição de mais de uma fonte de luz pontual
    Referente à issue #7

--- include/cena/Cena.hpp
+++ include/cena/Cena.hpp
@@ -16,7 +16,7 @@ class Cena {
         // Vetor com ponteiros para os sólidos presentes na cena.
         std::vector<std::unique_ptr<Solido>> solidos;
         // Luz pontual presente na cena.
-        std::unique_ptr<LuzPontual> fonte_luz;
+        std::vector<std::unique_ptr<LuzPontual>> fontes_luz;
         // Intensidade da luz ambiente.
         IntensidadeLuz I_A;
 
@@ -32,8 +32,6 @@ class Cena {
 
         // --- GETTERS E SETTERS ---
 
-        void set_fonte_luz(std::unique_ptr<LuzPontual> luz);
-
         Camera get_camera() const;
         void set_camera(Camera c);
 
@@ -44,6 +42,9 @@ class Cena {
 
         // Insere um novo sólido na cena.
         void inserir_solido(std::unique_ptr<Solido> solido);
+
+        // Insere uma nova fonte de luz na cena.
+        void inserir_fonte_luz(std::unique_ptr<LuzPontual> luz);
         
         // Chaca qual sólido o raio intersectou primeiro, calcula a interação da luz com o sólido e retorna a cor rgb correspondente.
         // Se o raio não intersectar nada, retorna a cor de fundo.
