ca1d81b2aeca311804b2d9b3ca3a15a6fbb50512
edcoelho <edcoelho.dev@gmail.com>
2023-12-09 15:11:08 -0300

    Faz uma pequena alteração na classe Triangulo

--- src/geometria/Triangulo.cpp
+++ src/geometria/Triangulo.cpp
@@ -4,19 +4,19 @@
 
 Triangulo::Triangulo(Material material) : Solido(material) {
 
-    this->set_vertice1(Ponto3(1.0, 0.0, 0.0));
-    this->set_vertice2(Ponto3(0.0, 1.0, 0.0));
-    this->set_vertice3(Ponto3(0.0, 0.0, 1.0));
+    this->vertice1 = Ponto3(1.0, 0.0, 0.0);
+    this->vertice2 = Ponto3(0.0, 1.0, 0.0);
+    this->vertice3 = Ponto3(0.0, 0.0, 1.0);
 
-    this->normal = (this->get_vertice2() - this->get_vertice1()).vetorial(this->get_vertice3() - this->get_vertice1()).unitario();
+    this->normal = (this->vertice2 - this->vertice1).vetorial(this->vertice3 - this->vertice1).unitario();
 
 }
 
 Triangulo::Triangulo(Ponto3 vertice1, Ponto3 vertice2, Ponto3 vertice3, Material material) : Solido(material) {
 
-    this->set_vertice1(vertice1);
-    this->set_vertice2(vertice2);
-    this->set_vertice3(vertice3);
+    this->vertice1 = vertice1;
+    this->vertice2 = vertice2;
+    this->vertice3 = vertice3;
 
     this->normal = (vertice2 - vertice1).vetorial(vertice3 - vertice1).unitario();
 
@@ -30,6 +30,7 @@ Ponto3 Triangulo::get_vertice1() const {
 void Triangulo::set_vertice1(Ponto3 v) {
 
     this->vertice1 = v;
+    this->normal = (vertice2 - vertice1).vetorial(vertice3 - vertice1).unitario();
 
 }
 
@@ -41,6 +42,7 @@ Ponto3 Triangulo::get_vertice2() const {
 void Triangulo::set_vertice2(Ponto3 v) {
 
     this->vertice2 = v;
+    this->normal = (vertice2 - vertice1).vetorial(vertice3 - vertice1).unitario();
 
 }
 
@@ -52,25 +54,28 @@ Ponto3 Triangulo::get_vertice3() const {
 void Triangulo::set_vertice3(Ponto3 v) {
 
     this->vertice3 = v;
+    this->normal = (vertice2 - vertice1).vetorial(vertice3 - vertice1).unitario();
 
 }
 
 double Triangulo::escalar_interseccao(Raio& raio) const {
 
     // Plano do triângulo.
-    Plano plano(this->get_vertice1(), this->normal, this->get_material());
+    Plano plano(this->vertice1, this->normal);
+
     // Escalar distância até a intersecção.
     double t_int;
+
+    // Área do triângulo vezes dois.
+    double area_triangulo_vezes_2;
+
     // Ponto de intersecção.
     Ponto3 p_int;
 
     // Vetores para auxiliar nos cálculos.
-
     Vetor3 r1, r2, v;
-    // Área do triângulo vezes dois.
-    double area_triangulo_vezes_2;
-    // Coordenadas baricêntricas.
 
+    // Coordenadas baricêntricas.
     double c1, c2, c3;
 
     t_int = plano.escalar_interseccao(raio);
@@ -103,6 +108,6 @@ double Triangulo::escalar_interseccao(Raio& raio) const {
 
 Vetor3 Triangulo::vetor_normal_ponto(Ponto3 ponto) const {
 
-    return (this->vertice2 - this->vertice1).vetorial(this->vertice3 - this->vertice1).unitario();
+    return this->normal;
 
 }
